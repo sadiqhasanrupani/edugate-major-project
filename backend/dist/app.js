5112792f871cb0b7964f392bbc35a3e605f02dc8
Sadiqhasan <sadiqhasanrupani11@gmail.com>
2023-02-01 21:02:50 +0530

    backend demo

---
+++ backend/dist/app.js
@@ -0,0 +1,24 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// Library Imports
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+// routes
+const login_1 = __importDefault(require("./routes/login"));
+const app = (0, express_1.default)();
+const port = Number(process.env.PORT);
+app.use(body_parser_1.default.json());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+app.use("/login", login_1.default);
+app.listen(port);
+//# sourceMappingURL=app.js.map
---
+++ backend/dist/routes/login.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const router = (0, express_1.Router)();
+router.get("/", (req, res, next) => {
+});
+exports.default = router;
+//# sourceMappingURL=login.js.map

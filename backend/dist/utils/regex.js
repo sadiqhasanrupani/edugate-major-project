1fa776849ab8a3248e088015d0c1e986ce13c24d
Sadiqhasan <sadiqhasanrupani11@gmail.com>
2023-02-18 14:12:39 +0530

    removed the dist folder

--- backend/dist/utils/regex.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dateRegex = exports.numRegex = exports.gmailRegex = void 0;
-exports.gmailRegex = /^[a-zA-Z0-9_.+-]+@gmail\.com$/;
-exports.numRegex = /^\+\d{1,3}\d{0,9}$/;
-exports.dateRegex = /^\d{4}-\d{2}-\d{2}$/;
-//# sourceMappingURL=regex.js.map

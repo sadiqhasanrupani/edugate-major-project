2764e03b494cec9ba3c674fdbc46f0e6a0d0bc5d
Sadiqhasan <sadiqhasanrupani11@gmail.com>
2023-02-20 22:43:04 +0530

    updated the database configuration

--- backend/src/utils/database.config.ts
+++ backend/src/utils/database.config.ts
@@ -2,29 +2,30 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
+const sequelize = new Sequelize(
+  process.env.SQL_DATABASE as string,
+  process.env.SQL_USER as string,
+  process.env.SQL_PASSWORD,
+  {
+    dialect: "mysql",
+    host: process.env.SQL_HOST,
+    port: Number(process.env.SQL_PORT)
+  }
+);
+
 // const sequelize = new Sequelize(
-//   "edugate_db",
+//   process.env.DATABASE as string,
 //   process.env.USER as string,
 //   process.env.PASSWORD,
 //   {
-//     dialect: "mysql",
 //     host: process.env.HOST,
+//     dialect: "mysql",
+//     dialectOptions: {
+//       ssl: {
+//         rejectUnauthorized: true
+//       },
+//     },
 //   }
 // );
-
-const sequelize = new Sequelize(
-  process.env.DATABASE as string,
-  process.env.USER as string,
-  process.env.PASSWORD,
-  {
-    host: process.env.HOST,
-    dialect: "mysql",
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized: true
-      },
-    },
-  }
-);
 
 export default sequelize;
